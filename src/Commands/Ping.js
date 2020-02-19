96edc01d95bdc3fb32ecf47b1b32f5c33c5b70c2
Kiio__ <caillaud.hugo.44@gmail.com>
2020-02-19 22:30:34 +0100

    Real bot latency

--- src/Commands/Ping.js
+++ src/Commands/Ping.js
@@ -10,10 +10,10 @@ class Ping extends Command {
    * Play pingpong with the bot
    * @param  {Discord.message} message User message
    */
-  static action(message, Bot){
-    this.msg(message).sendBack('Pong :ping_pong:');
+  static async action(message, Bot){
+    let m = await this.msg(message).sendBack('Pong :ping_pong:');
     this.log(2, `Pinging ${this.getConfig().BOT.NAME}`);
-    this.log(4, `Bot ping is ${Bot.ping}`)
+    this.log(4, `Bot latency is ${m.createdTimestamp - message.createdTimestamp}, API latency is ${Bot.ping}`);
   }
 }
 
